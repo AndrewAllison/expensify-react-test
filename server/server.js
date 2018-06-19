bfc7b0e6e7a44a4fa376d2a40abc405a15fe0a18
Andy Allison <andy@talkbe.com>
2018-06-19 07:03:11 +0100

    build(Prod): Get ready for a prod build

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+// Set up server
+app.use(express.static(publicPath));
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
+});
